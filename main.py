2866013fa0df48bcb709165f9a7a0c5bbede6342
Stuy-Kevin-Lin <klin14@stuy.edu>
2019-06-09 17:11:25 -0400

    Reflection testing

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from Sphere import *
 from Vector import *
 from Ray import *
 
+def getColor(ray, objlst, numBounces):
+    if numBounces <= 0:
+        return Vector([0,0,0])
+    closest = None
+    closestObj = None
+    for i in objlst:
+        currT = i.isIntersect(ray)
+        if closest == None or (currT != None and currT < closest):
+            closest = currT
+            closestObj = i
+    if closest == None:
+        return Vector([0,0,0])
+    else:
+        reflectedRay = closestObj.getReflected(ray, ray.pointAtT(closest))
+        return Vector([10,10,10]) + getColor(reflectedRay, objlst, numBounces - 1)
+
 '''
 if len(sys.argv) == 2:
     run(sys.argv[1])
@@ -18,7 +34,6 @@ screen = new_screen()
 color = [ 255, 0, 0 ]
 vals = []
 
-
 #Making the center as a vector allows me to use my overloaded operators
 test = Sphere(Vector([250,250,100]),50)
 objlst = [test, Sphere(Vector([50, 50, 30]), 20), Sphere(Vector([330, 250, 100]), 30)]
@@ -30,39 +45,11 @@ objlst = [test, Sphere(Vector([50, 50, 30]), 20), Sphere(Vector([330, 250, 100])
 #         print( startRay.pointAtT(inters) )
 #         print( objlst[1].getReflected(startRay, startRay.pointAtT(inters) ) )
 #     print('------')
+
 for x in range(501):
     for y in range(501):
         #print(test.isIntersect(z,[250,250,0]))
         firedRay = Ray(Vector([x,y,0]),Vector([0,0,1]))
-        closest = None
-        closestObj = None
-        for i in objlst:
-            currT = i.isIntersect(firedRay)
-            # if x == y == 250:
-            #     print(currT)
-            if closest == None or (currT != None and currT < closest):
-                closest = currT
-                closestObj = i
-        if closest != None:
-            intersection = firedRay.pointAtT(closest)
-            # print(val)
-            plot(screen,zbuff,color,x,y,1)
-            reflectedRay = closestObj.getReflected(firedRay, intersection)
-            # if x == y == 50:
-            #     print(reflectedRay)
-            secondRefl = None
-            closestObj2 = None
-            for i in objlst:
-                currT = i.isIntersect(reflectedRay)
-                if secondRefl == None or (currT != None and currT < closest):
-                    secondRefl = currT
-                    closestObj2 = i
-            if secondRefl != None:
-                # if closestObj == objlst[1]:
-                    # print(closestObj2)
-                    # print(secondRefl)
-                    # print(reflectedRay)
-                    # print(closestObj.getReflected(reflectedRay, reflectedRay.pointAtT(secondRefl)))
-                    # print('--------------')
-                plot(screen,zbuff,[0, 255, 0],x,y,1)
+        colorVector = getColor(firedRay, objlst, 5)
+        plot(screen,zbuff,colorVector.direction,x,y,1)
 display(screen)
